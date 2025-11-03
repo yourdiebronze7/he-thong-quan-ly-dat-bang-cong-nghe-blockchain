93905e034ffde4208d4d1ba752e6e9a7b60821ef
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-11-03 10:43:26 +0000

    Updated welcome message for clarity

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Land Registration Management System');
+  res.send('Welcome to the Land Registration Management System. Your gateway to efficient land management.');
 });
 
 app.listen(port, () => {
